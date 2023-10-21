2f1afa8f49b9064121bfab838adf90b5e977844d
AMOSA500 <66817828+AMOSA500@users.noreply.github.com>
2023-10-21 22:34:12 +0000

    Code line simplified and checks if shift amount is perfectly divided without remainder

--- main.py
+++ main.py
@@ -10,35 +10,33 @@ def cipher(text, shift_amount, direction):
   encrypted_text = ""
   direction_dict = {1:"encode", 2:"decode"}
   #Encode here
-  if direction_dict[direction] == "encode": 
-    for x in text:
-      x = x.lower()
-      if x in alphabets:
-        position = alphabets.index(x)
-        shift_position = position + shift_amount
-        if shift_position > 25:
-          r_char = alphabets[shift_position%25]
-          encrypted_text += r_char
-        else:
-          r_char = alphabets[shift_position]
-          encrypted_text += r_char
-      else:
-        encrypted_text += x
+  if direction_dict[direction] == "encode":
+    shift_amount = shift_amount
+  elif direction_dict[direction] == "decode":
+    shift_amount *= -1
   else:
-    #Decode here
-    for x in text:
-      x = x.lower()
-      if x in alphabets:
-        position = alphabets.index(x)
-        shift_position = position + (shift_amount * -1)
-        if shift_position < 25:
-          r_char = alphabets[shift_position%25]
-          encrypted_text += r_char
-        else:
-          r_char = alphabets[shift_position]
-          encrypted_text += r_char
+    pass
+    
+  for x in text:
+    x = x.lower()
+    if x in alphabets:
+      position = alphabets.index(x)
+      shift_position = position + shift_amount
+      if shift_position > 25 or shift_position < 25:
+        # Check if shift_amount is perfectly divided.
+        if shift_amount%25 == 0:
+          if direction_dict[direction] == "encode":
+            shift_position = shift_position%25 + 1
+          else:
+            shift_position = shift_position%25 - 1
+        r_char = alphabets[shift_position%25]
+        encrypted_text += r_char
       else:
-        encrypted_text += x
+        r_char = alphabets[shift_position]
+        encrypted_text += r_char
+    else:
+      encrypted_text += x
+  
 
   print(f"{text} was encode to {encrypted_text}")
 
